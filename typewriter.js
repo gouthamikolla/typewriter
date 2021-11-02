9db65e4d476d37498d2ab95d0921a095691c7b61
Gouthami Kolla <89610131+gouthamikolla@users.noreply.github.com>
2021-11-02 18:33:59 -0400

    Typewriter with setTimeOut

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "hello there from lighthouse labs \n";
+/*for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  },1000);
+}
+*/
+
+let delay = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  },delay);
+  delay += 50;
+}
